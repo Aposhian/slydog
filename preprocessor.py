ef69eb5c4524cddb5be3efa9b38899ec941a3a07
Adam Aposhian <adam.l.aposhian@gmail.com>
2019-03-30 10:30:36 -0600

    Added character initialization and wall sprites

--- preprocessor.py
+++ preprocessor.py
@@ -1,17 +1,20 @@
 import re
 import string
-from pathlib import Path
 
-path = "testScript.txt"
+class Preprocessor:
+    def __init__(self, characters):
+        self.characters = characters
+        self.id_to_index = {}
 
-characters = {'0':,'1':,'2':,'3':,'4':}
+    def getMatch(match):
+        id = match.group(0).translate(str.maketrans('','','{}'))
+        return characters[id_to_index[id]].name
 
-def getMatch(match):
-    id = match.group(0).translate(str.maketrans('','','{}'))
-    return characters[id].name
-
-with open(path) as file:
-    content = file.read()
-    s = re.sub('\{.+\}', getMatch, content)
-    print(s)
+    def replace(self, path, id_to_index_dictionary):
+        id_to_index = id_to_index_dictionary
+        with open(path) as file:
+            content = file.read()
+            s = re.sub('\{.+\}', getMatch, content)
+            id_to_index = {}
+            return s
 
